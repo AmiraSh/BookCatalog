dd37ef6aebb1b39461171ebf83a58681b9650903
AmiraSh <amira.sheremet@gmail.com>
2016-02-24 20:19:54 +0200

    added Filter and Operator clesses to perform filtration

---
+++ BookCatalog/BookCatalog.Infrastructure/Filtration/Operator.cs
@@ -0,0 +1,33 @@
+﻿namespace BookCatalog.Infrastructure.Filtration
+{
+    /// <summary>
+    /// Custom operator for filtering.
+    /// </summary>
+    public enum CustomOperator
+    {
+        /// <summary>
+        /// Contains.
+        /// </summary>
+        Contains,
+
+        /// <summary>
+        /// Is greater then.
+        /// </summary>
+        Greater,
+
+        /// <summary>
+        /// Is less then.
+        /// </summary>
+        Less,
+
+        /// <summary>
+        /// Is greater then or equal.
+        /// </summary>
+        GreaterOrEqual,
+
+        /// <summary>
+        /// Is less then or equal.
+        /// </summary>
+        LessOrEqual
+    }
+}
